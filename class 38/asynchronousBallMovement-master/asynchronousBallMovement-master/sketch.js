7f1ee18f7ecb5ec1af33b6d592794011fc05f6a0
Kdhiraj1234 <79121722+Kdhiraj1234@users.noreply.github.com>
2021-05-17 20:13:33 -0400

    Add files via upload

---
+++ class 38/asynchronousBallMovement-master/asynchronousBallMovement-master/sketch.js
@@ -0,0 +1,23 @@
+var canvas,backgroundImg;
+var gameState = 0;
+var playerCount,database,form,game,player;
+var allPlayers = [];
+
+function setup(){
+    database= firebase.database();
+    canvas = createCanvas(400,400);
+
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    if(playerCount === 4){
+        game.update(1);
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
+}
